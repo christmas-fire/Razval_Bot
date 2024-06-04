b695950d8cd068c976d981848a7aa20e3a0a512a
christmas-fire <vpiskin335@gmail.com>
2024-06-05 00:31:14 +0700

    Готова команда /gallery

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ from os import getenv
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, html, types, F
 from aiogram.filters import Command
-from aiogram.types import Message, CallbackQuery
+from aiogram.types import Message, CallbackQuery, FSInputFile
 from aiogram.enums import ParseMode
+from aiogram.utils.media_group import MediaGroupBuilder
 
 from text_handler import *
 from default_commands import *
@@ -25,11 +26,12 @@ pm = ParseMode.HTML
 
 @dp.message(Command(commands=["start"]))
 async def command_start_handler(message: Message) -> None:
-    picture = types.FSInputFile("images/cat_start.jpg")
+    picture = FSInputFile("images/cat_start.jpg")
     text = text_command_start()
     
     await message.answer_photo(picture, text, parse_mode=pm, reply_markup=inline_start())
     await add_user(message.chat.id, message.from_user.username, message.from_user.first_name)
+    
 
 @dp.callback_query(F.data.startswith("start_"))
 async def callback_start(callback: CallbackQuery):
@@ -40,6 +42,40 @@ async def callback_start(callback: CallbackQuery):
     if action == "contacts":
         await callback.message.answer(text, parse_mode=pm, disable_web_page_preview=True)
 
+    await callback.answer()
+    
+
+@dp.message(Command(commands=["gallery"]))
+async def command_gallery(message: Message) -> None:
+    picture = FSInputFile("images/cat_gallery.jpg")
+    text = text_command_gallery()
+    
+    await message.answer_photo(picture, text, parse_mode=pm, reply_markup=inline_gallery())
+    
+    
+@dp.callback_query(F.data.startswith("gallery_"))
+async def callback_start(callback: CallbackQuery):
+    action = callback.data.split("_")[1]
+    
+    if action == "tatoo":
+        album = MediaGroupBuilder(caption=text_inline_gallery_tatoo())
+        for i in range(1, 4):
+            album.add_photo(media=FSInputFile(f"images/gallery_tatoo_{i}.jpg"))
+
+        await callback.message.answer_media_group(media=album.build())
+        await callback.message.answer(reply_markup=inline_gallery_if_tatoo(), text="------")
+    
+    elif action == "draw":
+        album = MediaGroupBuilder(caption=text_inline_gallery_draw())
+        for i in range(1, 4):
+            album.add_photo(media=FSInputFile(f"images/gallery_draw_{i}.jpg"))
+
+        await callback.message.answer_media_group(media=album.build())
+        await callback.message.answer(reply_markup=inline_gallery_if_draw(), text="------")
+        
+    elif action == "back":
+        await command_gallery(callback.message)
+    
     await callback.answer()
     
     
