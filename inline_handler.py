95ccc26edb195147c943ec33b3932ec3f6c4b9a2
christmas-fire <vpiskin335@gmail.com>
2024-06-05 00:30:34 +0700

    Создал клавиатуру для команды /gallery

--- inline_handler.py
+++ inline_handler.py
@@ -9,3 +9,32 @@ def inline_start() -> InlineKeyboardMarkup:
         ]
     )
     return kb
+
+def inline_gallery() -> InlineKeyboardMarkup:
+    kb = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text="🖊Эскизы татуировок", callback_data="gallery_tatoo"),
+            InlineKeyboardButton(text="🎨Рисунки", callback_data="gallery_draw")]
+        ]
+    )
+    return kb
+
+
+def inline_gallery_if_tatoo() -> InlineKeyboardMarkup:
+    kb = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text="◀️Вернуться", callback_data="gallery_back"),
+             InlineKeyboardButton(text="🎨Рисунки", callback_data="gallery_draw")]
+        ]
+    )
+    return kb
+
+
+def inline_gallery_if_draw() -> InlineKeyboardMarkup:
+    kb = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text="◀️Вернуться", callback_data="gallery_back"),
+             InlineKeyboardButton(text="🖊Эскизы татуировок", callback_data="gallery_tatoo")]
+        ]
+    )
+    return kb
