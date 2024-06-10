a3182efafc5f87e66ecb7efc1d733a4ba37a23ee
christmas-fire <vpiskin335@gmail.com>
2024-06-10 17:02:01 +0700

    Отложил работу с таблицей orders на потом, добавил время

--- sql_handler.py
+++ sql_handler.py
@@ -1,22 +1,32 @@
 import sqlite3 as sq
+from datetime import datetime
 
+current_time = datetime.now().replace(microsecond=0)
 
 async def db_start() -> None:
     global db, cur
     
-    db = sq.connect("MassageAssistant.db")
+    db = sq.connect("RazvalBot.db")
     cur = db.cursor()
 
     cur.execute(
         """CREATE TABLE IF NOT EXISTS users(
-        id INTEGER,
+        id INTEGER PRIMARY KEY,
         username TEXT,
         name TEXT)
-                """
+        """
     )
 
-    db.commit()
+    cur.execute(
+        """CREATE TABLE IF NOT EXISTS orders(
+        order_id INTEGER PRIMARY KEY,
+        user_id INTEGER,
+        date DATETIME,
+        FOREIGN KEY (user_id) REFERENCES users (id))
+        """
+    )
     
+    db.commit()
     
 async def add_user(id, username, name):
     user = cur.execute(f"SELECT 1 FROM users WHERE id == {id}").fetchone()
